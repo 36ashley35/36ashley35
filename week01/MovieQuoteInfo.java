408f10511eb084b56a31a6a67a5bb405905507d9
36ashley35 <158353525+36ashley35@users.noreply.github.com>
2024-09-01 21:44:54 -0500

    Create MovieQuoteInfo.java

---
+++ week01/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+public class MovieQuote {
+    public static void main(String[] args) {
+        // Print the movie quote
+        System.out.println("Hold me. Promise me that you won't let go.");
+        // Print the character who said the quote
+        System.out.println("said by Lonnie");
+        // Print the movie title
+        System.out.println("in the movie Surviving");
+        // Print the year of the movie
+        System.out.println("in 1985");
+    }
+}
